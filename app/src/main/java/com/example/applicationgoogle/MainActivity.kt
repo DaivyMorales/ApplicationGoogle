fa67420c601c6c41bcb187308e1947d7f8528ddf
unknown <SENA@MOSAPRCBAFSD091.FormacionSena.RED>
2023-02-25 10:20:08 -0500

    Creando proyecto

---
+++ app/src/main/java/com/example/applicationgoogle/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.applicationgoogle
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.applicationgoogle.ui.theme.ApplicationGoogleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ApplicationGoogleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ApplicationGoogleTheme {
+        Greeting("Android")
+    }
+}
